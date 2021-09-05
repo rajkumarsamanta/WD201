c3b1434e721b630ce0d4ea8c2df55983ec45c01c
rajkumarsamanta <rajkumar.samanta@bcrec.ac.in>
2021-09-06 00:25:21 +0530

    Learn integer string float boolean types

--- Ruby_Test.rb
+++ Ruby_Test.rb
@@ -1,8 +1,35 @@
 # Ask for a list of TODOs 
 # and print them back to us.
 
-puts "What is your next todo ?"
-next_todo = gets
+# puts "What is your next todo ?"
+# next_todo = gets
 
-puts "\n\n\n" 
-puts "[ ] #{next_todo}"   # String Interpolation 
+# puts "\n\n\n" 
+# puts "[ ] #{next_todo}"   # String Interpolation 
+
+# Learning Ruby Variable Types
+count=100
+course="SaaS 201"
+is_live=true
+lessons=['Basics','Intermediate','Advanced']
+puts "\n\n"
+puts "This is "+course+" #{lessons[1]} course"
+
+length=20
+breadth=30
+area = length*breadth
+puts "The area of a rectangle with Length = #{length} and Breadth = #{breadth} is: #{area}"
+
+# Learning BOOLEAN
+
+puts "Fiction or non-fiction?"
+genre = gets.chomp.downcase
+
+if genre == "fiction"
+  puts "Try reading Cryptonomicon by Neal Stephenson"
+elsif genre == "non-fiction"
+  puts "You should read The Ascent of Man by Jacob Bronowski!"
+else
+  puts "Oh I don't know about that genre"
+end
+
