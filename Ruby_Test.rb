f792d25d655cddb9a80204ad45820f29f3b5123d
rajkumarsamanta <rajkumar.samanta@bcrec.ac.in>
2021-09-06 01:24:21 +0530

    Learn array and problem 1 nested array

--- Ruby_Test.rb
+++ Ruby_Test.rb
@@ -33,3 +33,15 @@ else
   puts "Oh I don't know about that genre"
 end
 
+#Learn Array
+my_nums=[1,2,3,4,5,6,7]
+my_nums.each{|x| puts "Current number is: #{x}"}
+new_nums=my_nums.map{|x| x**2}
+new_nums.each{|n| puts n}
+
+# Problem 1: nested array of names
+names=[["Raj Kumar","Samanta"],["Vivek", "Sood"],["P. K.","Sethy"],["Hirok","Dutta"]]
+full_names=names.map{|x| x[0]+ " " +x [1]}
+full_names.each{|n| puts n}
+
+
