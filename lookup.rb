d66a3b6df3269bd372c2df8fa04e9b481dc70ffc
rajkumarsamanta <rajkumar.samanta@bcrec.ac.in>
2021-09-11 01:31:36 +0530

    Improve dns resolver

--- lookup.rb
+++ lookup.rb
@@ -2,7 +2,6 @@ def get_command_line_argument
   # ARGV is an array that Ruby defines for us,
   # which contains all the arguments we passed to it
   # when invoking the script from the command line.
-  # https://docs.ruby-lang.org/en/2.4.0/ARGF.html
   if ARGV.empty?
     puts "Usage: ruby lookup.rb <domain>"
     exit
@@ -15,36 +14,23 @@ domain = get_command_line_argument
 
 # File.readlines reads a file and returns an
 # array of string, where each element is a line
-# https://www.rubydoc.info/stdlib/core/IO:readlines
 dns_raw = File.readlines("zone")
 
-# FILL YOUR CODE HERE
-# FILL YOUR CODE HERE
-def parse_dns(dns_data)
-  records={}
-  dns_data.each do |line|
-      entryNo=records.length
-      dns_row ={}
-      if line.length > 1
-          if line[0] != "#"
-              entryNo = entryNo + 1
-              items = line.split(",")
-              items.each_with_index{|x,i| items[i] = x.strip}
-              dns_row[entryNo] = { :type => items[0], :source => items[1], :destination => items[2] }
-              records =records.merge!(dns_row)
-          end
-      end
+def parse_dns(raw)
+  raw.
+  reject {|line| line.include?("#") || line.strip.empty? }.
+  map {|line| line.strip.split(", ") }.
+  each_with_object({}) do |record, records|
+      records[record]={ :type => record[0], :source => record[1], :destination => record[2] }
   end
-  return records
 end
 
-#dns_records = parse_dns(dns_raw)
 def resolve(dns_records, lookup_chain, domain)
-  entry = dns_records.select{|recNo, data| data[:source] == domain }
+  entry = dns_records.select{|key, data| data[:source] == domain }
   if entry.empty?() == true
       lookup_chain.push("ERROR!! records not found for #{domain}")
   else 
-      entry.each do |recNo, data|
+      entry.each do |key, data|
           if data[:type]=="A"
               lookup_chain.push(data[:destination])
           else
@@ -56,14 +42,6 @@ def resolve(dns_records, lookup_chain, domain)
   end
   return lookup_chain
 end    
-
-# FILL YOUR CODE HERE
-
-# FILL YOUR CODE HERE
-
-# To complete the assignment, implement `parse_dns` and `resolve`.
-# Remember to implement them above this line since in Ruby
-# you can invoke a function only after it is defined.
 dns_records = parse_dns(dns_raw)
 lookup_chain = [domain]
 lookup_chain = resolve(dns_records, lookup_chain, domain)
