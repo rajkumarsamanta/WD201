e94e48ee1c3af4a1839c9ba9f138ba16a9f3cde6
rajkumarsamanta <rajkumar.samanta@bcrec.ac.in>
2021-09-18 14:22:20 +0530

    Build todo management CLI app

--- L4-ActiveRecord/list_todos.rb
+++ L4-ActiveRecord/list_todos.rb
@@ -2,5 +2,12 @@ require './connect_db.rb'
 require './todo.rb'
 connect_db!
 
-displayable_list = Todo.all.map{|todo| todo.to_displayable_string}
-puts displayable_list
+puts "\nMy Todo-list \n\n"
+puts "Overdue\n"
+Todo.where("due_date < ?",Date.today).show_list
+
+puts "\nDue Today\n"
+Todo.where("due_date = ?",Date.today).show_list
+
+puts "\nDue Later\n"
+Todo.where("due_date > ?",Date.today).show_list
--- L4-ActiveRecord/todo.rb
+++ L4-ActiveRecord/todo.rb
@@ -5,13 +5,38 @@ class Todo < ActiveRecord::Base
         due_date == Date.today
     end
 
+    def overdue?
+        due_date < Date.today
+    end 
+
+    def due_later?
+        due_date > Date.today
+    end
+
     def to_displayable_string
         display_status = completed ? "[X]" : "[ ]"
         display_date = due_today? ? nil : due_date
-        "#{display_status} #{todo_text} #{display_date}"
+        "#{id}. #{display_status} #{todo_text} #{display_date}"
     end
 
     def self.to_displayable_list
         all.map{|todo| todo.to_displayable_string }
     end
+
+    def self.show_list
+        displayable_list = Todo.all.map{|todo| todo.to_displayable_string}
+        puts displayable_list
+    end
+
+    def self.add_task(h)
+        Todo.create!(todo_text:h[:todo_text],due_date:Date.today + h[:due_in_days], completed:false).id
+    end
+
+    def self.mark_as_complete(todo_id)
+        todo =Todo.find(todo_id)
+        todo.completed = !todo.completed
+        todo.save   
+        Todo.find(todo_id)
+    end
+
 end 
